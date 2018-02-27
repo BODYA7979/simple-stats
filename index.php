01d012d80c69034c1e5b4912aafa927cefa5384b
conky <b.konkevych@internetdevels.com>
2018-02-27 12:40:51 +0200

    Working, Rewritten router to slim framework

--- index.php
+++ index.php
@@ -1,76 +1,50 @@
 <?php
 require_once 'start.php';
 
-$url_parts = parse_url($_SERVER['REQUEST_URI']);
-$path_parts = explode('/', ltrim($url_parts['path'], '/'));
-$response = [];
-if (!empty($path_parts)) {
-  if ($path_parts[0] == 'stats') {
-    if ($path_parts[1] == 'get') {
-      // route callback: /stats/get/top/all-time/{entity_type}
-      if ($path_parts[2] == 'top' && $path_parts[3] == 'all-time' && !empty($path_parts[4])) {
-        $entity_type = $path_parts[4];
-        $limit = (isset($_GET['limit'])) ? intval($_GET['limit']) : 10;
-        $response = $stats->topAllTimeByEntityType($entity_type, $limit);
-      }
-      // /stats/get/top/{entity_type}/{timestamp_from}/{timestamp_to} [?limit={limit}]
-      elseif ($path_parts[2] == 'top' && !empty($path_parts[3]) && !empty($path_parts[4]) && !empty($path_parts[5])) {
-        $entity_type = $path_parts[3];
-        $from = $path_parts[4];
-        $to = $path_parts[5];
-        $limit = (isset($_GET['limit'])) ? intval($_GET['limit']) : FALSE;
-        $results = $stats->topEntitiesByEntityTypeAndTimestamp($entity_type, $from, $to, $limit);
-        $response = $results;
-      }
-      else {
-        // route callback: /stats/get/{entity_type}/{entity_id}
-        if (!empty($path_parts[2]) && !empty($path_parts[3])) {
-          // show total view results for selected entity
-          $entity_type = $path_parts[2];
-          $entity_id = $path_parts[3];
-          $data = (!empty($_GET['data']) ? $_GET['data'] : []);
-          if ($count = $stats->get($entity_type, $entity_id, $data)) {
-            $response['status'] = 'OK';
-            $response['count'] = $count;
-          }
-          else {
-            $response['status'] = 'FAIL';
-            $response['code'] = Errors::STATS_NO_DATA;
-            $response['message'] = Errors::getMessage(Errors::STATS_NO_DATA);
-          }
-        }
-      }
-    }
-    elseif ($path_parts[1] == 'write') {
-      if (!empty($path_parts[2]) && !empty($path_parts[3])) {
-        // add plus one count
-        $entity_type = $path_parts[2];
-        $entity_id = $path_parts[3];
-        $data = (!empty($_GET['data']) ? $_GET['data'] : []);
-        $stats->write($entity_type, $entity_id, $data);
-        $response['status'] = 'OK';
-      }
-    }
-    elseif ($path_parts[1] == 'trigger') {
-      if (!empty($path_parts[2]) && !empty($path_parts[3])) {
-        // we need to add plus one to views count and show results
-        $entity_type = $path_parts[2];
-        $entity_id = $path_parts[3];
-        $data = (!empty($_GET['data']) ? $_GET['data'] : []);
-        $stats->write($entity_type, $entity_id, $data);
-        $count = $stats->get($entity_type, $entity_id, $data);
-        $response['status'] = 'OK';
-        $response['count'] = $count;
-      }
-    }
-  }
-}
+$app = new Slim\App();
 
-if (!empty($response)) {
-  http_response_code(200);
-  header('Content-Type: application/json');
-  echo json_encode($response);
-}
-else {
-  http_response_code(404);
-}
+$app->get('/stats/get/top/all-time/{entity_type}', function ($request, $response, $args) use ($stats) {
+  /** @var $response \Slim\Http\Response */
+  /** @var $request \Slim\Http\Request */
+  return $response->withJson($stats->topAllTimeByEntityType($args['entity_type'], $request->getQueryParam('limit', 10)), 200);
+});
+
+$app->get('/stats/get/top/{entity_type}/{timestamp_from}/{timestamp_to}', function ($request, $response, $args) use ($stats) {
+  /** @var $response \Slim\Http\Response */
+  /** @var $request \Slim\Http\Request */
+  return $response->withJson($stats->topEntitiesByEntityTypeAndTimestamp($args['entity_type'], $args['timestamp_from'], $args['timestamp_to'], $request->getQueryParam('limit', FALSE)), 200);
+});
+
+$app->get('/stats/get/{entity_type}/{entity_id}', function ($request, $response, $args) use ($stats) {
+  /** @var $response \Slim\Http\Response */
+  /** @var $request \Slim\Http\Request */
+  $return = [];
+  $data = $request->getQueryParam('data', []);
+  if ($count = $stats->get($args['entity_type'], $args['entity_id'], $data)) {
+    $return['status'] = 'OK';
+    $return['count'] = $count;
+  }
+  else {
+    $return['status'] = 'FAIL';
+    $return['code'] = Errors::STATS_NO_DATA;
+    $return['message'] = Errors::getMessage(Errors::STATS_NO_DATA);
+  }
+  return $response->withJson($return, 200);
+});
+
+$app->get('/stats/write/{entity_type}/{entity_id}', function ($request, $response, $args) use ($stats) {
+  /** @var $response \Slim\Http\Response */
+  /** @var $request \Slim\Http\Request */
+  $stats->write($args['entity_type'], $args['entity_id'], $request->getQueryParam('data', []));
+  return $response->withJson(['status' => 'OK'], 200);
+});
+
+$app->get('/stats/trigger/{entity_type}/{entity_id}', function ($request, $response, $args) use ($stats) {
+  /** @var $response \Slim\Http\Response */
+  /** @var $request \Slim\Http\Request */
+  $stats->write($args['entity_type'], $args['entity_id'], $request->getQueryParam('data', []));
+  $count = $stats->get($args['entity_type'], $args['entity_id'], $request->getQueryParam('data', []));
+  return $response->withJson(['status' => 'OK', 'count' => $count], 200);
+});
+
+$app->run();
